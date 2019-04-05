cfe2a9434a1d345646ce548fba1be0b15df065f3
Brendon Le Comte <brendon.lecomte@gmail.com>
2019-04-05 16:58:47 +0100

    start filling in mmu

--- src/mmu.h
+++ src/mmu.h
@@ -4,16 +4,25 @@
 #include "cartridge.h"
 #include "cpu.h"
 
+class SerialPort {
+public:
+  SerialPort(){};
+  ~SerialPort(){};
+  uint8_t read(){};
+  void write(const uint8_t data) { std::cout << data << std : endl; }
+}
+
 class MMU {
 public:
-  MMU(Cartridge &cart) : m_cart(cart){};
+  MMU(CartridgeMemory &cart_memory) : m_cart(cart_memory), m_serialPort(){};
   ~MMU(){};
-  uint8_t read8bit(const uint16_t address){m_cart.read(address)};
-  uint16_t read16bit(const uint16_t address){};
+  uint8_t read8bit(const uint16_t address);
+  uint16_t read16bit(const uint16_t address);
 
-  void write(const uint16_t address, const uint8_t data){};
-  void write(const uint16_t address, const uint16_t data){};
+  void write(const uint16_t address, const uint8_t data);
+  void write(const uint16_t address, const uint16_t data);
 
 private:
-  Cartridge &m_cart;
+  Cartridge &m_cartridge;
+  SerialPort m_serialPort;
 }
