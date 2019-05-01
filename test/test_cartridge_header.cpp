d81b2038d56921df0014cf2d9bf4aa8dd73cc5bd
Brendon Le Comte <brendon.lecomte@gmail.com>
2019-05-01 14:04:15 +0100

    add local cart test. might fail on travis due to lack of rom

--- test/test_cartridge_header.cpp
+++ test/test_cartridge_header.cpp
@@ -9,6 +9,7 @@ class CartridgeHeaderTest : public ::testing::Test {
  protected:
   // You can remove any or all of the following functions if its body
   // is empty.
+  Cartridge cart = Cartridge("../roms/Tetris.gb");
 
   CartridgeHeaderTest() {
      // You can do set-up work for each test here.
@@ -33,5 +34,9 @@ class CartridgeHeaderTest : public ::testing::Test {
 
   // Objects declared here can be used by all tests in the test case for Foo.
 };
+
+TEST_F(CartridgeHeaderTest, type) {
+  EXPECT_EQ(ROM_ONLY, cart.cart_header.getCartType());
+}
 
 }
