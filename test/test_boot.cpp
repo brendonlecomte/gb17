c008b271fad84d13aa91cbad05e030ea160a6e88
Brendon Le Comte <brendon.lecomte@gmail.com>
2019-05-15 18:31:19 +0100

    add test to lock down CPU state post boot rom. Current appears correct

---
+++ test/test_boot.cpp
@@ -0,0 +1,62 @@
+#include "gtest/gtest.h"
+#include "../src/CPU/CPU.h"
+#include "../src/MMU/MMU.h"
+#include "../src/MMU/Cartridge.h"
+#include "../src/PPU/PPU.h"
+
+namespace {
+
+// The fixture for testing class Foo.
+class BootTest : public ::testing::Test {
+protected:
+  // You can remove any or all of the following functions if its body
+  // is empty.
+
+  Cartridge game_cart = Cartridge("../../gb-test-roms/cpu_instrs/individual/01-special.gb");
+  MMU memory_manager = MMU(game_cart.getMemoryController());
+  CPU test_cpu = CPU(memory_manager, NULL);
+  PPU test_ppu = PPU(memory_manager);
+
+  BootTest() {
+
+  }
+
+  ~BootTest() override {
+    // You can do clean-up work that doesn't throw exceptions here.
+  }
+
+  // If the constructor and destructor are not enough for setting up
+  // and cleaning up each test, you can define the following methods:
+
+  void SetUp() override {
+    // Code here will be called immediately after the constructor (right
+    // before each test).
+  }
+
+  void TearDown() override {
+    // Code here will be called immediately after each test (right
+    // before the destructor).
+  }
+
+  // Objects declared here can be used by all tests in the test case for Foo.
+};
+
+TEST_F(BootTest, run_boot_rom) {
+  while(1) {
+    unsigned saved_pc = test_cpu.PC;
+    OpCode op = test_cpu.readOp();
+    uint32_t clocks = test_cpu.execute_op(op);
+    test_ppu.update(clocks);
+    if(test_cpu.PC == 0x100 || test_cpu.PC > 0x1000) {
+      break;
+    }
+  }
+  EXPECT_EQ(0x0100,   (uint16_t)test_cpu.PC);
+  EXPECT_EQ(0xFFFE,  (uint16_t)test_cpu.SP);
+  EXPECT_EQ(0x0100,  (uint16_t)test_cpu.AF);
+  EXPECT_EQ(0x0013,  (uint16_t)test_cpu.BC);
+  EXPECT_EQ(0x00D8,  (uint16_t)test_cpu.DE);
+  EXPECT_EQ(0x014D,  (uint16_t)test_cpu.HL);
+}
+
+} // namespace
