dd884fa109060d3592d0d8b095251860f457c31f
Brendon Le Comte <brendon.lecomte@gmail.com>
2019-05-29 13:49:40 +0100

    new banked memory class

---
+++ src/MMU/BankedMemory.h
@@ -0,0 +1,40 @@
+#pragma once
+#include <stdint.h>
+#include <vector>
+
+class BankMemory {
+public:
+  BankMemory(uint16_t bank_size, uint16_t bank_count) : m_size(bank_size), m_bank(1) {
+    memory.resize(bank_count);
+    for(uint16_t i =0; i < bank_count; i++) {
+      memory[i].resize(bank_size);
+    }
+  };
+
+  ~BankMemory() {};
+
+  void setBank(uint16_t bank) { m_bank = bank; }
+
+  //new [] operators...
+  uint8_t& operator[](uint16_t address) { return memory[bankSelect(address)][convertAddress(address)]; };
+  // const uint8_t& operator[](uint16_t address) const { return memory[bankSelect(address)][convertAddress(address)]; };
+
+private:
+  uint16_t bankSelect(uint16_t address) {
+    if(address >= m_size) {
+      return m_bank;
+    }
+    return 0;
+  };
+
+  uint16_t convertAddress(uint16_t address) {
+    if(address >= m_size) {
+      return address;
+    }
+    return address & ~m_size; //clears the bank size bit
+  };
+
+  std::vector< std::vector<uint8_t> > memory;
+  uint16_t m_bank;
+  uint16_t m_size;
+};
---
+++ test/test_banking.cpp
@@ -0,0 +1,50 @@
+#include "../src/MMU/BankedMemory.h"
+#include "gtest/gtest.h"
+
+namespace {
+
+// The fixture for testing class Foo.
+class BankTest : public ::testing::Test {
+protected:
+  // You can remove any or all of the following functions if its body
+  // is empty.
+  BankMemory mem = BankMemory(0x1000, 4);
+
+  BankTest() {
+
+  }
+
+  ~BankTest() override {
+    // You can do clean-up work that doesn't throw exceptions here.
+  }
+
+  // If the constructor and destructor are not enough for setting up
+  // and cleaning up each test, you can define the following methods:
+
+  void SetUp() override {
+    // Code here will be called immediately after the constructor (right
+    // before each test).
+  }
+
+  void TearDown() override {
+    // Code here will be called immediately after each test (right
+    // before the destructor).
+  }
+
+  // Objects declared here can be used by all tests in the test case for Foo.
+};
+
+TEST_F(BankTest, writeReadTest) {
+  mem[0xFF] = 0xAA;
+  EXPECT_EQ(mem[0xFF], 0xAA);
+  EXPECT_EQ(mem[0x55], 0x00);
+
+  EXPECT_EQ(mem[0x10FF], 0x00);
+  mem[0x10FF] = 0xAA;
+  EXPECT_EQ(mem[0x10FF], 0xAA);
+
+  mem.setBank(2);
+  EXPECT_EQ(mem[0x10FF], 0x00);
+  EXPECT_EQ(mem[0xFF], 0xAA);
+}
+}
