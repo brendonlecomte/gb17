5e895bc8550b37aa6ac461301ac73b4e3087e55f
Brendon Le Comte <brendon.lecomte@gmail.com>
2019-05-29 14:30:37 +0100

    serial port extracted from mmu

--- src/MMU/MMU.h
+++ src/MMU/MMU.h
@@ -3,20 +3,8 @@
 #include "../Timer/Timer.h"
 #include "BankedMemory.h"
 #include "Cartridge.h"
+#include "SerialPort.h"
 #include <stdint.h>
-
-class SerialPort {
-public:
-  SerialPort(std::ostream *out = NULL) : serial(out){};
-  ~SerialPort(){};
-  void setControl(const uint8_t data){};
-  uint8_t getControl() { return 0; };
-  uint8_t read() { return 0; };
-  void write(const uint8_t data) { *serial << char(data); }
-
-private:
-  std::ostream *serial;
-};
 
 class MMU {
 public:
---
+++ src/MMU/SerialPort.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <stdint.h>
+
+
+class SerialPort {
+public:
+  SerialPort(std::ostream *out = NULL) : serial(out){};
+  ~SerialPort(){};
+  void setControl(const uint8_t data){};
+  uint8_t getControl() { return 0; };
+  uint8_t read() { return 0; };
+  void write(const uint8_t data) { *serial << char(data); }
+
+private:
+  std::ostream *serial;
+};
