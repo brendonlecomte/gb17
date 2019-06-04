3017b1f557553608528109d4c2d25eff5239bff8
Brendon Le Comte <brendon.lecomte@gmail.com>
2019-06-04 14:51:21 +0100

    improved cartridge constructor. ram now created

--- src/MMU/Cartridge.cpp
+++ src/MMU/Cartridge.cpp
@@ -1,44 +1,46 @@
-#include <assert.h>
-#include <iterator>
 #include "Cartridge.h"
 #include "Cartridge_Header.h"
-#include "ROMOnly.h"
 #include "MBC1.h"
+#include "ROMOnly.h"
+#include <assert.h>
+#include <iterator>
 
 const uint16_t rom_bank = 0x4000;
 
 void Cartridge::createController(void) {
-  switch (cart_header.getCartType())
-  {
+  switch (cart_header.getCartType()) {
     case CartType::ROM_ONLY:
       memory_controller = new RomOnlyCart(rom);
       break;
     case CartType::MBC1:
-      memory_controller = new MBC1(rom, 2);
+      memory_controller =
+          new MBC1(rom, cart_header.getRomBanks(), ram, cart_header.getRamSize(), cart_header.getRamBanks());
     default:
       break;
   }
 }
 
-Cartridge::Cartridge(const char* filename) {
+Cartridge::Cartridge(const char *filename) {
   std::ifstream file(filename, std::ifstream::in);
-  //ensure file exists
-  if(!file) assert(0);
+  // ensure file exists
+  if (!file)
+    assert(0);
 
   // Stop eating new lines in binary mode!!!
   file.unsetf(std::ios::skipws);
 
-  //Create the CartType
+  // Create the CartType
   file.seekg(0, std::ios::beg);
   file.seekg(0x100);
-  cart_header.populate(file); //TODO: replace this with using the rom array post read.
+  cart_header.populate(file); // TODO: replace this with using the rom array post read.
 
   file.seekg(0, std::ios::end);
   uint32_t length = file.tellg();
-  rom = new uint8_t[length]; //create rom memory the same size as the cart
+  rom = new uint8_t[length]; // create rom memory the same size as the cart
 
   file.seekg(0, std::ios::beg);
-  file.read((char*)rom, length); //populate the array
+  file.read((char *)rom, length); // populate the array
 
+  ram = new uint8_t[cart_header.getRamSize()];
   createController();
 }
